37b166a170e55c2a33cf1f2a38874b81c7c02537
vpaklatzis <vpaklatzis@gmail.com>
2021-04-15 20:15:18 +0000

    Server and client side changes to implement the final project requirements

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
 const app = new express();
 
 app.use(express.static('client'))
@@ -11,7 +13,6 @@ app.get("/",(req,res)=>{
   });
 
 app.get("/url/emotion", (req,res) => {
-
     return res.send({"happy":"90","sad":"10"});
 });
 
@@ -31,3 +32,19 @@ let server = app.listen(8080, () => {
     console.log('Listening', server.address().port)
 })
 
+function getNLUInstance() {
+    let api_key = process.env.API_KEY;
+    let api_url = process.env.API_URL;
+
+    const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+    const { IamAuthenticator } = require('ibm-watson/auth');
+
+    const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+        version: '2020-08-01',
+        authenticator: new IamAuthenticator({
+            apikey: api_key,
+        }),
+        serviceUrl: api_url
+    });
+    return naturalLanguageUnderstanding;
+}
